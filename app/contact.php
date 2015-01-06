8520cd09ed50e6430d54e00d63c71aa438f22482
Abdulrezak <abdulrazaq_18@hotmail.com>
2015-01-06 13:58:22 +0000

    changes to subscribtion stuff

--- app/contact.php
+++ app/contact.php
@@ -1,15 +1,9 @@
 <?php
-/*
- * A Design by W3layouts
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
- *
- */			
- 						$userEmail=$_REQUEST['userEmail'];
+include "config.php";			
+ 						
+                                                $userEmail=$_REQUEST['userEmail'];
 						$webadd= "http://". $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-						$subject = "new subscription received ".$webadd;  
+						$subject = "new subscription received ".$webadd;
 			$message = '<html><head><title>'.$subject.'</title></head><body><table><tr><td>Email id :  </td><td>'.$userEmail.'</td></tr><tr><td></td><td> '.$subject.'</td>
 </tr></table></body></html>';
 			//$message = "Email id :  ".$userEmail. "\r\nPhone No : ".$userPhone."\r\nName : ".$userName."\r\nSays : ".$userMsg;
@@ -21,11 +15,11 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 			$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 			if(!mail($to, $subject, $message, $headers)){
              $mail_status='no';
-				header("Location: index.html");
+				header("Location: /index.html");
 			exit();
           }else{
           	  $mail_status='yes';
-          	header("Location: index.html");
+          	header("Location: /index.html");
 			exit();
            
          } ?>
